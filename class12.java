05bf3defb80b7bead1fe0e0feaa5baeed0114b17
“Bin111360119” <barrywang222@gmail.com>
2024-10-30 00:24:50 +0800

    123

---
+++ class12.java
@@ -0,0 +1,17 @@
+package hw2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class class12 {
+
+	public static void main(String[] args)throws IOException
+	{
+		int i=1;
+		do  {
+			System.out.println("第"+i+"次迴圈");
+			i++;
+		}while(i<=5);
+		System.out.println("迴圈結束");
+	}
+}
